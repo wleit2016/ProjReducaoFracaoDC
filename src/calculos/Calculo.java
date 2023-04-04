224e430b8269ce24290eb0955dfc1bce20268b94
wleit2016 <wenderleite2008@gmail.com>
2023-04-04 12:09:03 -0300

    O projeto esta finalizado.

--- src/calculos/Calculo.java
+++ src/calculos/Calculo.java
@@ -40,9 +40,60 @@ public class Calculo {
 		return resposta;
 	}
 	
-	public void fazerMMC(int[] denominadores, int numeroPrimo) {
-		while (denominadores[0] != 1 && denominadores[1] != 1 && denominadores[2] != 1) {
-			if(denominadores[0] % numeroPrimo == 0 && denominadores[1] % numeroPrimo == 0) {}
+	public int retorneMMC(int[] deno, int numeroPrimo) {
+		int[] denominadores = new int[3];
+		boolean condicao = true;
+		int mmc = 1;
+		
+		denominadores = deno.clone();
+		
+		while (condicao) {
+			if(eDivisivel(denominadores[0], numeroPrimo) && eDivisivel(denominadores[1], numeroPrimo) && eDivisivel(denominadores[2], numeroPrimo)) {
+				denominadores[0] = denominadores[0] / numeroPrimo;
+				denominadores[1] = denominadores[1] / numeroPrimo;
+				denominadores[2] = denominadores[2] / numeroPrimo;
+				mmc = mmc * numeroPrimo;
+				
+			} else if(eDivisivel(denominadores[0], numeroPrimo) && eDivisivel(denominadores[1], numeroPrimo)) {
+				denominadores[0] = denominadores[0] / numeroPrimo;
+				denominadores[1] = denominadores[1] / numeroPrimo;
+				mmc = mmc * numeroPrimo;
+				
+			} else if(eDivisivel(denominadores[1], numeroPrimo) && eDivisivel(denominadores[2], numeroPrimo)) {
+				denominadores[2] = denominadores[2] / numeroPrimo;
+				denominadores[1] = denominadores[1] / numeroPrimo;
+				mmc = mmc * numeroPrimo;
+				
+			} else if(eDivisivel(denominadores[0], numeroPrimo) && eDivisivel(denominadores[2], numeroPrimo)) {
+				denominadores[0] = denominadores[0] / numeroPrimo;
+				denominadores[2] = denominadores[2] / numeroPrimo;
+				mmc = mmc * numeroPrimo;
+				
+			} else {
+				int posicao = 0;
+				int trocarNumeroPrimo = 0;
+				
+				while(posicao < 3) {
+					if(eDivisivel(denominadores[posicao], numeroPrimo)) {
+						denominadores[posicao] = denominadores[posicao] / numeroPrimo;
+						trocarNumeroPrimo++;
+						
+					}
+					posicao++;
+				}
+				
+				if(trocarNumeroPrimo == 0) {
+					numeroPrimo = proximoPrimo(numeroPrimo);
+				} else {
+					mmc = mmc * numeroPrimo;
+				}
+			}
+			
+			if(denominadores[0] == 1 && denominadores[1] == 1 && denominadores[2] == 1) {
+				condicao = false;
+			}
+			
 		}
+		return mmc;
 	}
 }
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -13,8 +13,11 @@ public class Principal {
 		int[] numerador = new int[3];
 		int[] denominador = new int [3];
 		int numeroPrimo = 2;
+		int mmc = 0;
+		int posicao = 0;
+		int somaNumerador = 0;
 		
-		System.out.println("Informe 3 frações para fazer a redução de denominador comum.\n");
+		System.out.println("Programa soma de fraçoes com denominadores diferentes.\n");
 		
 		for(int i=0; i <3; i++) {
 			System.out.printf("Informe o %d numerador: ", i + 1);
@@ -27,8 +30,31 @@ public class Principal {
 		
 		Calculo calculo = new Calculo();
 		
-		System.out.println(calculo.ePrimo(11));
-		System.out.println(calculo.proximoPrimo(11));
+		System.out.println("Frações informadas:\n");
+		System.out.println(numerador[0] + "/" + denominador[0] + "+" + numerador[1] + "/" + denominador[1] + "+" + numerador[2] + "/" + denominador[2]);
+		
+		mmc = calculo.retorneMMC(denominador, numeroPrimo);
+		
+		System.out.println("O MMC dos denominadores: " + mmc);
+		
+		while(posicao < 3) {
+			denominador[posicao] = mmc / denominador[posicao];
+			numerador[posicao] = numerador[posicao] * denominador[posicao];
+			posicao++;
+		}
+		
+		System.out.println("==> " + numerador[0] + "/" + denominador[0] + "+" + numerador[1] + "/" + denominador[1] + "+" + numerador[2] + "/" + denominador[2]);
+		
+		System.out.println("==> " + numerador[0] + "/" + mmc + "+" + numerador[1] + "/" + mmc + "+" + numerador[2] + "/" + mmc);
+		
+		posicao = 0;
+		
+		while(posicao < 3) {
+			somaNumerador += numerador[posicao];
+			posicao++;
+		}
+		
+		System.out.println("\nResultado => " + somaNumerador + "/" + mmc);
 	}
 
 }
